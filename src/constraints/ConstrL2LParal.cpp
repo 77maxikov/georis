24b221a6d481ede3e64b9275b511aa7bb0679de3
77maxikov <77maxikov@mail.ru>
2022-04-26 09:36:02 +0300

    Fixed parallel constraint

--- src/constraints/ConstrL2LParal.cpp
+++ src/constraints/ConstrL2LParal.cpp
@@ -10,9 +10,8 @@ double georis::ConstrL2LParal::error()const{
 
 	if ( a1x*a1x + a1y*a1y < epsi*epsi ) throw std::runtime_error("ConstrL2LParal::not a line 1!");
 	if ( a2x*a2x + a2y*a2y < epsi*epsi ) throw std::runtime_error("ConstrL2LParal::not a line 2!");
-	double er = a1x*a2y - a1y*a2x;
 
-	return er*er;
+    return  a1x*a2y - a1y*a2x;
 }
 double georis::ConstrL2LParal::grad(const paramProxy *var)const{
     double a1x = *l12x->pval - *l11x->pval;
@@ -23,24 +22,22 @@ double georis::ConstrL2LParal::grad(const paramProxy *var)const{
 	if ( a1x*a1x + a1y*a1y < epsi*epsi ) throw std::runtime_error("ConstrL2LParal::not a line 1!");
 	if ( a2x*a2x + a2y*a2y < epsi*epsi ) throw std::runtime_error("ConstrL2LParal::not a line 2!");
 
-	double er = a1x*a2y - a1y*a2x;
-
     if (var == l11x)
-        return 2*er*(-a2y);
+        return (-a2y);
     if (var == l11y)
-        return 2*er*a2x;
+        return a2x;
     if (var == l12x)
-        return 2*er*a2y;
+        return a2y;
     if (var == l12y)
-        return 2*er*(-a2x);
+        return (-a2x);
     if (var == l21x)
-        return 2*er*a1y;
+        return a1y;
     if (var == l21y)
-        return 2*er*(-a1x);
+        return (-a1x);
     if (var == l22x)
-        return 2*er*(-a1y);
+        return (-a1y);
     if (var == l22y)
-        return 2*er*a1x;
+        return a1x;
     return 0;
 }
 
